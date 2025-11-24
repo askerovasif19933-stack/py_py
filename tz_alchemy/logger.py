a8fe5d8c9f507b283ca8bffebac77b67b55b2be4
Askerov <askerovasif1993@mail.ru>
2025-11-24 20:16:53 +0300

    add loggining

---
+++ tz_alchemy/logger.py
@@ -0,0 +1,39 @@
+import logging
+
+
+
+def get_logger(name: str, log_file: str = 'add.log', level = logging.INFO):
+
+    """
+    Возвращает настроенный логгер.
+    - name: имя логгера (обычно __name__ модуля)
+    - log_file: имя файла для логов
+    - level: уровень логирования
+    """
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+
+    if not logger.handlers:
+
+        # консоль
+        consol_handler = logging.StreamHandler()
+        consol_handler.setLevel(level)
+
+        # файл
+
+        file_handler = logging.FileHandler(log_file, mode='a', encoding='utf-8')
+        file_handler.setLevel(level)
+
+
+        # фармат 
+
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(massage)s')
+
+        consol_handler.setFormatter(formatter)
+        file_handler.setFormatter(formatter)
+
+        logger.addHandler(consol_handler)
+        logger.addHandler(file_handler)
+
+    return logger
